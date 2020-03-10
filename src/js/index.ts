2db7c0606691c614b68458f043ecccc950296a1f
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2020-03-10 10:28:38 +0100

    Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,10 +3,29 @@ interface Person {
     lastName: string;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let user: Person = { firstName: "Jack", lastName: "Moe" };
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("wordInput");
+let selectValue: HTMLSelectElement = <HTMLSelectElement>document.getElementById("textChange");
+let output: HTMLElement = <HTMLElement> document.getElementById("content");
+let showNameButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("showNameButton");
+showNameButton.addEventListener("click", showWords);
+
+function showWords(): void {
+    let word: string = inputElement.value;
+    let choice: string = selectValue.value;
+    if (word.length > 0) {
+        if (choice == "toUpper") {
+            output.innerHTML = word.toUpperCase();    
+        }
+        if (choice == "toLower") {
+            output.innerHTML = word.toLowerCase();
+        }
+        if (choice == "individualSpacing") {
+            output.innerHTML = word.split("").join(" ");
+        }
+    }
+    else{
+        output.innerHTML = "empty"
+    }
+}
